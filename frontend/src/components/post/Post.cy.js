1ce8f88868f832376f234d91eea5ae4720c8f418
Mathew Parkinson <mathewgparkinson@gmail.com>
2023-04-27 14:29:11 +0100

    add author id into feed heading for visibility

--- frontend/src/components/post/Post.cy.js
+++ frontend/src/components/post/Post.cy.js
@@ -1,7 +1,17 @@
 import Post from './Post'
 
+const createdBy = () => {}
+const currentUser = () => {}
+
+
 describe("Post", () => {
   it('renders a post with a message', () => {
+    cy.mount(<Post post={{_id: 1, message: "Hello, world"}} />);
+    cy.get('[data-cy="post"]').should('contain.text', "Hello, world")
+  })
+
+
+  it('renders a post with a delete button wh', () => {
     cy.mount(<Post post={{_id: 1, message: "Hello, world"}} />);
     cy.get('[data-cy="post"]').should('contain.text', "Hello, world")
   })

466ed5b1b5696845fe2b6747c3b0dd62169c51dc
Jack Thacker <jackthacker89@gmail.com>
2023-05-03 14:11:42 +0100

    Add styles to other users/ profiles
    Co-authored-by: Noelia Perez Marin <NoeliaMarin@users.noreply.github.com>

--- frontend/src/components/OtherUserProfile/OtherUserProfile.jsx
+++ frontend/src/components/OtherUserProfile/OtherUserProfile.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+import styles from "./OtherUserProfile.css"
 
 const OtherUserProfile = () => {
-
-
   const otherUser = window.localStorage.getItem("otherUserID");
-  const friendsList = window.localStorage.getItem("userFriends").split(',');
-
+  const friendsList = window.localStorage.getItem("userFriends").split(",");
 
   return (
     <>
-
-        <div className="profile-container">
-          <img src={window.localStorage.getItem("image")}/>
-          <h1>{window.localStorage.getItem("username")}</h1>
-          {friendsList.includes(otherUser) ? 
-            <button>They are friends</button>
-           : <button>They are not friends</button>
-          }
-
-          {/* window.localStorage.getItem("friends").split(',').each(friend) => if userprofile ID is in friends array */}
+      <div className="profile-container">
+        <div className="profile-img-container">
+        <img className="profile-img" src={window.localStorage.getItem("image")} />
         </div>
+        <h1 className="profile-username">{window.localStorage.getItem("username")}</h1>
+        {friendsList.includes(otherUser) ? (
+          <button>You are friends</button>
+        ) : (
+          <button>You are not friends</button>
+        )}
+
+        {/* window.localStorage.getItem("friends").split(',').each(friend) => if userprofile ID is in friends array */}
+      </div>
       {/* if other userprofile.id is in current user friends array display unfriend + link to friends
       otherwise display add friend button */}
       {/* display feed of posts where post author id matches userprofile.id */}

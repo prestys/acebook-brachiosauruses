cea81450b41124cff121af0a7365d624434d5557
Jack Thacker <jackthacker89@gmail.com>
2023-05-03 16:44:50 +0100

    Add profile feed styles

--- frontend/src/components/OtherUserProfile/OtherUserProfile.jsx
+++ frontend/src/components/OtherUserProfile/OtherUserProfile.jsx
@@ -1,29 +1,27 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Post from "../post/Post";
 import "./OtherUserProfile.css";
-const OtherUserProfile = ({navigate}, props) => {
-
-
+const OtherUserProfile = ({ navigate }, props) => {
   const otherUser = window.localStorage.getItem("otherUserID");
   const userID = window.localStorage.getItem("userID");
-  const friendsList = window.localStorage.getItem("userFriends").split(',');
+  const friendsList = window.localStorage.getItem("userFriends").split(",");
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [otherUserID, setOtherUserID] = useState(window.localStorage.getItem("otherUserID"))
+  const [otherUserID, setOtherUserID] = useState(
+    window.localStorage.getItem("otherUserID")
+  );
   const post = props;
   const proFeed = [];
 
   const checkFriends = () => {
-    if (otherUser === userID){
-      return null
+    if (otherUser === userID) {
+      return null;
+    } else if (friendsList.includes(otherUser)) {
+      return <button>They are friends.</button>;
+    } else {
+      return <button>They are not friends.</button>;
     }
-    else if (friendsList.includes(otherUser)) {
-      return <button>They are friends.</button>
-    }
-    else {
-      return <button>They are not friends.</button>
-    }
-  }
+  };
 
   useEffect(() => {
     if (token) {
@@ -38,32 +36,35 @@ const OtherUserProfile = ({navigate}, props) => {
           setToken(window.localStorage.getItem("token"));
           setPosts(data.posts);
         });
-  }
-}, []);
+    }
+  }, []);
 
   return (
     <>
+      <div className="profile-container">
+        <div className="profile-img-container">
+          <img
+            className="profile-img"
+            src={window.localStorage.getItem("image")}
+          />
+        </div>
+        <h1 className="profile-username">
+          {window.localStorage.getItem("username")}
+        </h1>
+        {checkFriends()}
 
-        <div className="profile-container">
-          <div className="profile-img-container">
-        <img className="profile-img" src={window.localStorage.getItem("image")} />
-        </div>
-        <h1 className="profile-username">{window.localStorage.getItem("username")}</h1>
-          {checkFriends()}
-        </div>
 
         <div className="profile-feed-container">
-          {posts.forEach(post => {
-            if (post.author.id === otherUserID){
-              proFeed.push(post)
+          {posts.forEach((post) => {
+            if (post.author.id === otherUserID) {
+              proFeed.push(post);
             } else {
-              console.log(false)
+              console.log(false);
             }
           })}
         </div>
 
-        <section className="feed">
-
+        <section className="profile-feed">
           <div id="feed" role="feed" className="feed-posts">
             {proFeed.map((post) => (
               <Post
@@ -71,14 +72,17 @@ const OtherUserProfile = ({navigate}, props) => {
                 key={post._id}
                 token={token}
                 setToken={setToken}
-                userID = {userID}
+                userID={userID}
                 navigate={navigate}
               />
             ))}
           </div>
         </section>
+
+
+      </div>
     </>
   );
-}
+};
 
 export default OtherUserProfile;
